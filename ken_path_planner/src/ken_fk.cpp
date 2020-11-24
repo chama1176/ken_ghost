1c49a0983d2d6bad406a7fabd559d2feea938abc
Kaede Yakushigawa <kaede6120@gmail.com>
2020-11-25 01:24:35 +0900

    dh param

--- ken_path_planner/src/ken_fk.cpp
+++ ken_path_planner/src/ken_fk.cpp
@@ -19,35 +19,48 @@ KenFK::KenFK()
   std::cout << rotZ(M_PI / 4) << std::endl;
   std::cout << "transZ" << std::endl;
   std::cout << transZ(2.3) << std::endl;
+  std::cout << "dhT" << std::endl;
+  std::cout << dhT(2, 0.0, 3, M_PI / 4) << std::endl;
 }
 
 KenFK::~KenFK() {}
 
-Eigen::Matrix4d KenFK::dhT(const double a, const double alfa, const double d, const double theta) {}
-Eigen::Matrix4d KenFK::rotX(const double rad)
+Eigen::Matrix4d KenFK::dhT(const double a, const double alfa, const double d, const double theta)
 {
-  Eigen::Matrix3d R = Eigen::AngleAxisd(rad, Eigen::Vector3d::UnitX()).toRotationMatrix();
+  Matrix4d M;
+
+  M = transX(a) * rotX(alfa) * transZ(d) * rotZ(theta);
+
+  return M;
+}
+
+Matrix4d KenFK::rotX(const double rad)
+{
+  Matrix3d R = AngleAxisd(rad, Vector3d::UnitX()).toRotationMatrix();
   Matrix4d M;
   M.setIdentity();
   M.block<3, 3>(0, 0) = R;
   return M;
 }
-Eigen::Matrix4d KenFK::transX(const double m)
+
+Matrix4d KenFK::transX(const double m)
 {
   Matrix4d M;
   M.setIdentity();
   M(0, 3) = m;
   return M;
 }
-Eigen::Matrix4d KenFK::rotZ(const double rad)
+
+Matrix4d KenFK::rotZ(const double rad)
 {
-  Eigen::Matrix3d R = Eigen::AngleAxisd(rad, Eigen::Vector3d::UnitZ()).toRotationMatrix();
+  Matrix3d R = AngleAxisd(rad, Vector3d::UnitZ()).toRotationMatrix();
   Matrix4d M;
   M.setIdentity();
   M.block<3, 3>(0, 0) = R;
   return M;
 }
-Eigen::Matrix4d KenFK::transZ(const double m)
+
+Matrix4d KenFK::transZ(const double m)
 {
   Matrix4d M;
   M.setIdentity();
